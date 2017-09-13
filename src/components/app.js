b124df8f236debc74b7463de0c3159d06b3b66bd
Stacey Astoveza <staceyastoveza@transfix.io>
2017-09-13 16:30:07 -0400

    search functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,11 @@
 class App {
   constructor() {
+  	this.restaurants = []
   }
+
+   // input button and eventListener on main page
+  	//  callback function when event gets fired to fetch the list of restaurants
+  	//  will then call the adapter(searches) then run getSearches
+  	//   - add a ".then" to save promise data
+
 }
---
+++ src/components/restaurant.js
@@ -0,0 +1,11 @@
+class Restaurant {
+  constructor(restaurantJSON) {
+    debugger
+    this.body = restaurantJSON.body
+    this.id = restaurantJSON.id
+  }
+
+  render() {
+    return `<li data-restaurantid='${this.id}' data-props='${JSON.stringify(this)}' class='seach-element'>${this.body} <i data-action='delete-restaurant' class="em em-scream_cat"></i></li>`
+  }
+}
