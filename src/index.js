20753e780be6552d30b5a809b188e6dda8de850b
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-09-14 11:12:54 -0400

    FILTERING IS WORKING WOOOOOOOOOO

--- src/index.js
+++ src/index.js
@@ -1,32 +1,46 @@
-const app = new App()
-const adapter = new SearchesAdapter()
+document.addEventListener("DOMContentLoaded", function() {
+
+	const app = new App()
+
+	const adapter = new SearchesAdapter()
 
 	const saveSearch = document.getElementById('search')
 
-  	saveSearch.addEventListener('click', restaurantHandler)
+	const searchTerm = document.getElementById('new-search-body')
+
+	adapter.getSearches()
+	.then((response) => {
+		app.restaurants = []
+		// console.log(response)
+		response.forEach(function(restaurant) {
+				app.restaurants.push(restaurant)
+		})
+	})
+	saveSearch.addEventListener('click', restaurantHandler)
+		// 	app.restaurants.push(restaurant))
+		// console.log(app.restaurants)
+		// const allRestaurants = app.restaurants
+		// const zip = searchTerm.value
+		// console.log(zip)
+		// const restaurantByZip = allRestaurants.filter(restaurant => restaurant.zipcode == zip)
+
+	function restaurantHandler() {
+		event.preventDefault()
+
+		app.filterRestaurantsByZip(searchTerm.value)
+		searchTerm.value = ""
+		// const filteredRestaurants = app.restaurants.filter(restaurant => restaurant.zipcode == searchTerm.value)
 
 
+		// console.log(app.restaurants)
+		// console.log(filteredRestaurants)
+		// function render() {
+		// 	return restaurant.restaurantNode.innerHTML = `<ul>${restaurant.restaurantHTML()}</ul>`
+		// }
+		console.log(app.restaurants)
+	}
 
-  function restaurantHandler() {
-  	event.preventDefault()
-  	adapter.getSearches()
-  	.then((response) => {
-  		// console.log(response)
-  		response.forEach(restaurant => app.restaurants.push(restaurant)) 
-  	// .then((response) => restaurants.render.bind(restaurant))
-  		console.log(restuarant)
-  	})
-
-  	// function render() {
-  	// 	return restaurant.restaurantNode.innerHTML = `<ul>${restaurant.restaurantHTML()}</ul>`
-  	// }
-  }
-
-
-
-
-
-
+})
 
 
 
