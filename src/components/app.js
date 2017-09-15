5dd1ea4ab387576f6432cb8f3b0853cc499eb9e2
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-09-15 10:54:55 -0400

    fixed filtering issue

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 class App {
   constructor() {
   	this.restaurants = []
+    this.filtered = null
   }
 
   filterRestaurantsByZip(zipcode) {
-     this.restaurants = this.restaurants.filter(restaurant => restaurant.zipcode == zipcode)
+     this.filtered = this.restaurants.filter(restaurant => restaurant.zipcode == zipcode)
   }
    // input button and eventListener on main page
   	//  callback function when event gets fired to fetch the list of restaurants
